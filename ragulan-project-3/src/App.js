9ea92f68b8d6be4554f608c3de3d84cebf45d532
ragulan <ragulanravi21@gmail.com>
2021-03-09 18:55:14 -0500

    set up fetch api call and app.js state

--- ragulan-project-3/src/App.js
+++ ragulan-project-3/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import Photo from './Photo'
 
+const mainUrl = `https://api.unsplash.com/photos`
+const searchUrl = `https://api.unsplash.com/search/photos`
+
+// setting the state of the photos going to be a empty array
+// setting the state so default its not loading
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const fetchImages = async () => {
+    setLoading(true)
+    // url make be changing because it use may search or just get the default images on load
+    let url
+    url = `${mainUrl}?client_id=CodMP8r22yWvgC9SCYoAh9X-dEMQLKt6zKPO-vNiJ3w`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  // using useeffect so it only runs when the app loads
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
+  return <h1> Stockify</h1>
 }
 
-export default App;
+export default App
