b72b76b56610542cdc6119d639753b7d59ec239c
ragulan <ragulanravi21@gmail.com>
2021-03-09 19:43:57 -0500

    added photo.js component that has image, user that posted, likes

--- ragulan-project-3/src/Photo.js
+++ ragulan-project-3/src/Photo.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const Photo = () => {
-  return <div></div>
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: { name, portfolio_url },
+}) => {
+  return (
+    <section>
+      <img src='{regular}' alt='alt_description' />
+      <div className='photoDetails'>
+        <h5>{name}</h5>
+        <p>{likes}</p>
+        <a href={portfolio_url}></a>
+      </div>
+    </section>
+  )
 }
 
 export default Photo
